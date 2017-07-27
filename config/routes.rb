8de5706bc92f524ba07117df9f27610241a0dfe9
Jordan Hudgens <jordan@devcamp.com>
2017-07-27 16:05:17 +0000

    Added Album

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :albums
   resources :blogs
   get 'pages/home'
 
---
+++ db/migrate/20170727160419_create_albums.rb
@@ -0,0 +1,12 @@
+class CreateAlbums < ActiveRecord::Migration[5.0]
+  def change
+    create_table :albums do |t|
+      t.string :title
+      t.string :subtitle
+      t.text :body
+      t.text :main_image
+
+      t.timestamps
+    end
+  end
+end
