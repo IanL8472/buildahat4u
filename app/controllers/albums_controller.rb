76afb96655b632694b9141e335f91f6ab656ce4f
Jordan Hudgens <jordan@devcamp.com>
2017-07-27 16:55:20 +0000

    Integrated new and create for album

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,4 +2,21 @@ class AlbumsController < ApplicationController
   def index
     @album_items = Album.all
   end
+  
+  def new
+    @album_items = Album.new
+  end
+  
+  def create
+    @album_items = Album.new(params.require(:album).permit(:title, :subtitle, :body))
+    
+    respond_to do |format|
+      if @album_items.save
+        format.html { redirect_to albums_path, notice: 'Picture added successfully' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+  
 end
